cb7e6020e78abf2bdea463d8dc407eaab0fdc34d
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2025-05-27 00:45:53 +0100

    wrote a really basic async runtime. got tons of stuff to do to make a ready for MaoMao

---
+++ build.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // We will also create a module for our other entry point, 'main.zig'.
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_mod.addImport("FStd_lib", lib_mod);
+
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "FStd",
+        .root_module = lib_mod,
+    });
+
+    b.installArtifact(lib);
+
+    const exe = b.addExecutable(.{
+        .name = "FStdTesting",
+        .root_module = exe_mod,
+    });
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
---
+++ src/modules/kyoto.zig
@@ -0,0 +1,96 @@
+//Kyoto is a Async Runtime. The name is inspired by Tokio the async runtime library of Rust.
+//Kyoto implements async using futures just like in rust.
+//TODO: Implement Future Chaining
+//TOOD: Implement Awaiting Mulitple Futures
+
+const std = @import("std");
+const testing = std.testing;
+const Poll = union(enum) {
+    Pending,
+    Finished: *anyopaque,
+};
+
+pub const Future = struct {
+    ptr: *anyopaque,
+    vtable: *const VTable,
+    const VTable = struct {
+        poll: *const fn (ptr: *anyopaque) Poll,
+    };
+
+    pub fn poll(self: Future) Poll {
+        return self.vtable.poll(self.ptr);
+    }
+};
+
+const Self = @This();
+
+allocator: std.mem.Allocator,
+futures: std.ArrayList(Future),
+
+pub fn init(allocator: std.mem.Allocator) Self {
+    return .{
+        .allocator = allocator,
+        .futures = .init(allocator),
+    };
+}
+pub fn schedule(self: *Self, future: Future) !void {
+    try self.futures.append(future);
+}
+
+pub fn deinit(self: *Self) void {
+    self.futures.deinit();
+}
+
+fn done(self: *Self) bool {
+    return self.futures.items.len == 0;
+}
+
+pub fn run(self: *Self) void {
+    while (!self.done()) {
+        for (self.futures.items, 0..) |future, idx| {
+            const res = future.poll();
+            switch (res) {
+                .Pending => continue,
+                .Finished => {
+                    _ = self.futures.swapRemove(idx);
+                },
+            }
+        }
+    }
+}
+
+test "KyotoSimple" {
+    const allocator = testing.allocator;
+    var kyoto = Self.init(allocator);
+    defer kyoto.deinit();
+
+    const Printer = struct {
+        value: i64 = 0,
+
+        const SelfP = @This();
+        pub fn poll(ctx: *anyopaque) Poll {
+            const self: *SelfP = @ptrCast(@alignCast(ctx));
+            if (self.value < 10) {
+                std.debug.print("Pending: {d}\n", .{self.value});
+                self.value += 1;
+                return Poll.Pending;
+            } else {
+                std.debug.print("Finished: {d}\n", .{self.value});
+                return Poll{ .Finished = self };
+            }
+        }
+
+        pub fn future(self: *SelfP) Future {
+            return .{
+                .ptr = self,
+                .vtable = &.{ .poll = SelfP.poll },
+            };
+        }
+    };
+    var p = Printer{};
+    var p2 = Printer{ .value = -50 };
+    try kyoto.schedule(p.future());
+    try kyoto.schedule(p2.future());
+    kyoto.run();
+    try testing.expect(true);
+}
