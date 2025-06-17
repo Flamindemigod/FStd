5ab3d22b7a6852a32ee36daff459b0214373b7a0
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2025-06-17 20:37:43 +0100

    just changed kyoto -> Kyoto to make nameing more consistent across modules

--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
-pub const Kyoto = @import("./modules/kyoto.zig");
+pub const Kyoto = @import("./modules/Kyoto.zig");
 pub const Arboroboros = @import("./modules/Arboroboros.zig");
 
 test "tests" {
     _ = @import("./modules/Arboroboros.zig");
-    _ = @import("./modules/kyoto.zig");
+    _ = @import("./modules/Kyoto.zig");
 }
